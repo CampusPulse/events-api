3d521570064d5856f4859cbcd980f765abe687a8
Adrian Edwards <adrian@adriancedwards.com>
2024-02-25 21:32:36 -0500

    first pass at reading data from a file

--- flask-server.py
+++ flask-server.py
@@ -1,19 +1,30 @@
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from schema import NormalizedEvent
+from pathlib import Path
+
+from campuspulse_event_ingest_schema import NormalizedEvent
 
 app = Flask(__name__) 
 CORS(app)
 
+alldata = []
+
 @app.route('/campus-pulse-api', methods = ['GET'])  
 def hello():
-    name = request.args.get('name')
-    if name is None:
-        text = 'Hello!'
-    else: 
-        text = 'Hello ' + name + '!'
 
-    return jsonify({"message": text})
+    return jsonify(alldata)
 
 if __name__ == '__main__':
+
+    input_dir = Path("./data")
+    
+    for datafile in input_dir.glob("*.parsed.normalized.ndjson"):
+        if not datafile.is_file():
+            continue
+
+        for line in datafile.read_text():   
+            alldata.append(NormalizedEvent.parse_obj(json.loads(line)))
+
+
     app.run(debug=True, port=3500)
