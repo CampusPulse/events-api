82d3b3b083298cfd57d28bcc28cd4c2b72c71ff4
jgafnea <84107636+jgafnea@users.noreply.github.com>
2025-02-11 13:53:08 -0600

    chore: sort imports

--- flask-server.py
+++ flask-server.py
@@ -1,17 +1,18 @@
-from flask import Flask, Response, jsonify, request, send_file, request as frequest
-from flask_cors import CORS
-from pathlib import Path
-from icalendar import Calendar, Event
 import json
-
-from campuspulse_event_ingest_schema import NormalizedEvent
-from datetime import datetime, date, timezone
 import logging
 import os
+from datetime import date, datetime, timezone
 from functools import wraps
+from pathlib import Path
+
+from campuspulse_event_ingest_schema import NormalizedEvent
+from flask import Flask, Response, jsonify, request, send_file
+from flask import request as frequest
+from flask_cors import CORS
+from icalendar import Calendar, Event
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__) 
+app = Flask(__name__)
 CORS(app)
 
 alldata = []
@@ -25,30 +26,35 @@ archive_dir = Path("./data/archive")
 if not archive_dir.exists():
     archive_dir.mkdir()
 
+
 def check_auth(username, password):
     expected_credential = os.getenv("UPLOAD_CREDENTIAL")
     if expected_credential is None or expected_credential == "":
         return False
-    return username == 'loader' and password == expected_credential
+    return username == "loader" and password == expected_credential
+
 
 def login_required(f):
-    """ basic auth for api """
+    """basic auth for api"""
+
     # https://stackoverflow.com/a/70317010/
     @wraps(f)
     def decorated_function(*args, **kwargs):
         auth = frequest.authorization
         if not auth or not check_auth(auth.username, auth.password):
-            return jsonify({'message': 'Authentication required'}), 401
+            return jsonify({"message": "Authentication required"}), 401
         return f(*args, **kwargs)
+
     return decorated_function
 
-@app.route('/v0/public.json', methods = ['GET'])  
-def public_json():
 
+@app.route("/v0/public.json", methods=["GET"])
+def public_json():
     return jsonify(alldata)
 
-@app.route('/v0/public.ics')
-def public_ics():    
+
+@app.route("/v0/public.ics")
+def public_ics():
     # Create a new calendar
     cal = Calendar()
 
@@ -56,52 +62,60 @@ def public_ics():
     for event in alldata:
         try:
             cal_event = Event()
-            cal_event.add('summary', event['title'])
-            cal_event.add('dtstart', event['start'])
-            cal_event.add('dtend', event['end'])
-            cal_event.add('description', event['description'])
-            
-            location = event['location']
-            location_str = ', '.join(filter(None, [
-                location.get('building'),
-                location.get('room_number'),
-                location.get('street'),
-                location.get('city'),
-                location.get('state'),
-                location.get('zipcode')
-            ]))
-            cal_event.add('location', location_str)
+            cal_event.add("summary", event["title"])
+            cal_event.add("dtstart", event["start"])
+            cal_event.add("dtend", event["end"])
+            cal_event.add("description", event["description"])
+
+            location = event["location"]
+            location_str = ", ".join(
+                filter(
+                    None,
+                    [
+                        location.get("building"),
+                        location.get("room_number"),
+                        location.get("street"),
+                        location.get("city"),
+                        location.get("state"),
+                        location.get("zipcode"),
+                    ],
+                )
+            )
+            cal_event.add("location", location_str)
             cal.add_component(cal_event)
         except Exception as e:
             print(e)
-        
+
     # Create a response object
-    response = Response(cal.to_ical(), mimetype='text/calendar')
-    response.headers['Content-Disposition'] = 'attachment; filename=calendar.ics'
-    
+    response = Response(cal.to_ical(), mimetype="text/calendar")
+    response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
+
     return response
 
+
 def archive_file(archive_filename, timestamp, archive_path):
-    archive_name = archive_path.with_name(f"{archive_filename.stem}_{timestamp}{archive_filename.suffix}")
+    archive_name = archive_path.with_name(
+        f"{archive_filename.stem}_{timestamp}{archive_filename.suffix}"
+    )
     archive_filename.rename(archive_name)
 
 
-
 def allowed_file(filename):
-    return '.' in filename and filename.endswith(".parsed.normalized.ndjson")
+    return "." in filename and filename.endswith(".parsed.normalized.ndjson")
 
-@app.route('/v0/import', methods=["POST"])
+
+@app.route("/v0/import", methods=["POST"])
 @login_required
 def upload():
     global alldata
     # check if the post request has the file part
-    if 'file' not in request.files:
-        return jsonify({'message': 'No Files Provided'}), 400
-    file = request.files['file']
+    if "file" not in request.files:
+        return jsonify({"message": "No Files Provided"}), 400
+    file = request.files["file"]
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
-    if file.filename == '':
-        return jsonify({'message': 'empty filename'}), 400
+    if file.filename == "":
+        return jsonify({"message": "empty filename"}), 400
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         destination = input_dir.joinpath(filename)
@@ -109,19 +123,21 @@ def upload():
 
         if temp_dest.exists():
             temp_dest.unlink()
-        
-        # file save parts    
+
+        # file save parts
         try:
-            file.save(temp_dest) 
+            file.save(temp_dest)
         except Exception as e:  # potential catch
             app.logger.error(f"Error saving file {destination}: {e}")
-            return jsonify({'message': 'Error saving file'}), 500 
+            return jsonify({"message": "Error saving file"}), 500
 
         tempsize = temp_dest.stat().st_size
         if tempsize < MINIMUM_FILE_UPLOAD_SIZE_BYTES:
-            app.logger.error(f"Error saving file {destination}: file size {tempsize} not greater than configured minimum {MINIMUM_FILE_UPLOAD_SIZE_BYTES}")
+            app.logger.error(
+                f"Error saving file {destination}: file size {tempsize} not greater than configured minimum {MINIMUM_FILE_UPLOAD_SIZE_BYTES}"
+            )
             temp_dest.unlink()
-            return jsonify({'message': 'File too small'}), 400 
+            return jsonify({"message": "File too small"}), 400
 
         if destination.exists():
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -132,7 +148,7 @@ def upload():
 
         # clean up older files from destination.parent
         for item in input_dir.iterdir():
-            if item!= destination:  # skip destination file
+            if item != destination:  # skip destination file
                 if item.is_file():
                     try:
                         item.unlink()
@@ -140,7 +156,7 @@ def upload():
                         app.logger.error(f"Error deleting file {item}: {e}")
         alldata = []
         update_data(input_dir)
-        return jsonify({'message': 'Success'}), 200
+        return jsonify({"message": "Success"}), 200
 
 
 def update_data(input_dir):
@@ -159,14 +175,18 @@ def update_data(input_dir):
                     if not event.start < datetime.now(timezone.utc):
                         alldata.append(event)
                 except TypeError as e:
-                    if str(e) != "can't compare offset-naive and offset-aware datetimes":
+                    if (
+                        str(e)
+                        != "can't compare offset-naive and offset-aware datetimes"
+                    ):
                         # app.logger.error(f"file {datafile }")
 
                         raise
                     tzun_count += 1
         if tzun_count >= 1:
-            app.logger.warning(f"file {datafile} contains {tzun_count} skipped timezone-unaware events")
-
+            app.logger.warning(
+                f"file {datafile} contains {tzun_count} skipped timezone-unaware events"
+            )
 
     alldata_tmp = sorted(alldata, key=lambda e: e.start)
 
@@ -176,14 +196,13 @@ def update_data(input_dir):
     app.logger.info("Processing event data complete")
 
 
-if __name__ != '__main__':
-
-    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+if __name__ != "__main__":
+    gunicorn_error_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_error_logger.handlers
     app.logger.setLevel(gunicorn_error_logger.level)
 
 
 update_data(input_dir)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
